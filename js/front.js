c8fa32957dd5061e1da716a319a8c9da1478c443
jvasquez0687 <jesus_06_06@hotmail.com>
2018-10-12 09:41:19 -0400

    hw

---
+++ js/front.js
@@ -0,0 +1,26 @@
+$(function () {
+
+// Navbar
+$(document).scroll(function () {
+    if ($(window).scrollTop() >= $('header').offset().top) {
+        $('nav').addClass('sticky');
+    } else {
+        $('nav').removeClass('sticky');
+    }
+});
+
+// Scroll Spy
+$('body').scrollspy({
+        target: '.navbar',
+        offset: 80
+});
+
+// Preventing URL update on navigation link click
+$('.navbar-nav a, #scroll-down').bind('click', function (e) {
+    var anchor = $(this);
+$('html, body').stop().animate({
+    scrollTop: $(anchor.attr('href')).offset().top
+    }, 1000);
+    e.preventDefault();
+});
+});
